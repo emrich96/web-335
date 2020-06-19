0b5f790d08bcb307a4d708f3d51a912d1bde4119
Emily Richter <emrichter1996@gmail.com>
2020-06-19 13:35:10 -0500

    Add week 8 files

---
+++ week-8/richter_calculator.py
@@ -0,0 +1,16 @@
+first_name='Emily'
+last_name='Richter'
+print(first_name+' '+last_name)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
---
+++ week-8/richter_hello_world.py
@@ -0,0 +1,5 @@
+first_name='Emily'
+last_name='Richter'
+print(first_name+' '+last_name)
+
+print('Hello World')
